f15248966710823f211408ba14c15079b7575643
s-odhiambo <samuelangienda1998@gmail.com>
2019-12-13 15:24:26 +0300

    Corrects the settings.py file.

--- tribune/settings.py
+++ tribune/settings.py
@@ -126,18 +126,18 @@ DATABASES = {
         'PASSWORD':'12345',
     }
 }
+{
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+{
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
